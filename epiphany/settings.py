a2ab7fdebd994d295b73441873b2accecfa92406
khushi-shah24 <khushishah2427@gmail.com>
2024-10-20 15:55:04 +0530

    commented cloudinary import

--- epiphany/settings.py
+++ epiphany/settings.py
@@ -13,8 +13,8 @@ import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 import dj_database_url
-import cloudinary
-import cloudinary_storage
+# import cloudinary
+# import cloudinary_storage
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 
 # Quick-start development settings - unsuitable for production
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 INSTALLED_APPS = [
