d9c631514793500f677c731af142569dedc5aadd
khushi-shah24 <khushishah2427@gmail.com>
2024-10-20 17:07:52 +0530

    added cloudinary in models.py

--- main/models.py
+++ main/models.py
@@ -1,23 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
+from cloudinary.models import CloudinaryField  # Import CloudinaryField
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    video = models.FileField(upload_to='videos/', null=True, blank=True)
+    
+    # Store videos in Cloudinary
+    video = CloudinaryField('video', blank=True, null=True)  # Replace FileField
+    
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    
+    # Store images in Cloudinary
+    image = CloudinaryField('image', blank=True, null=True)  # Replace ImageField
 
-        if self.image:
-            img = Image.open(self.image.path)
-            if img.height > 800 or img.width > 800:
-                output_size = (800, 800)
-                img = img.resize(output_size, Image.Resampling.LANCZOS)
-                img.save(self.image.path)
     def __str__(self):
         return self.title
 
